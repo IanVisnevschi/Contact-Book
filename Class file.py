59ee444157f61ec90107cc06b3cdc4aad7269fad
IanVisnevschi <22407525@bucks.ac.uk>
2025-12-02 10:39:22 +0000

    Add files via upload
    Bug fixes for the ContactApp class

--- Class file.py
+++ Class file.py
@@ -268,16 +268,16 @@ class ContactApp:
         self.entry_email = tk.Entry(root)
         self.entry_address = tk.Entry(root)
 
-        self.entry_first.grid(row = 0, colum = 1)
-        self.entry_last.grid(row = 0, colum = 2)
-        self.entry_phone.grid(row = 0, colum = 3)
-        self.entry_email.grid(row = 0, colum = 4)
-        self.entry_address.grid(row = 0, colum = 5)
+        self.entry_first.grid(row = 0, column = 1)
+        self.entry_last.grid(row = 1, column = 1)
+        self.entry_phone.grid(row = 2, column = 1)
+        self.entry_email.grid(row = 3, column = 1)
+        self.entry_address.grid(row = 4, column = 1)
 
         tk.Button(root, text = "Add Contact", command = self.add_contact).grid(row=5,column=0,pady=5)
         tk.Button(root, text = "show all contacts", command = self.show_contacts).grid(row=5,column=1)
         tk.Button(root, text = "Search Contact", command = self.search_contact).grid(row=6, column =0)
-        tk.Button(root, text = "Delete Contact", command = self.delete_contact).grid(row = 6 column = 1)
+        tk.Button(root, text = "Delete Contact", command = self.delete_contact).grid(row = 6, column = 1)
 
         self.output = tk.Text(root, height =15, width=50)
         self.output.grid(row=7,column = 0, columnspan = 2, pady = 10)
@@ -317,14 +317,14 @@ class ContactApp:
             return
         
         contacts_list = self.manager.contacts.to_list()
-        sorted_contacts = MergeSort._merge_sort(contacts_list,key=lambda c: c.first_name)
+        sorted_contacts = MergeSort.merge_sort(contacts_list,key=lambda c: c.first_name)
         result = BinarySearch.search(sorted_contacts, name, "first_name")
 
         self.output.delete("1.0",tk.END)
         if result:
             self.output.insert(tk.END,f"FOUND: {result}\n")
         else:
-            self.output.inser(tk.END,f"CONTACT NOT FOUND.\n")
+            self.output.insert(tk.END,f"CONTACT NOT FOUND.\n")
 
     
     def delete_contact(self):
